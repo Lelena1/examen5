73d88b6507b282468260b48d651e1b49e0729ea3
Lelena1 <luppol@inbox.ru>
2023-07-24 03:59:43 +0300

    Решена задача к билету 12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Collection<User> users = new ArrayList<>();
+        users.add(new User("Kate", 5));
+        users.add(new User("John", 15));
+        users.add(new User("Slava", 52));
+        users.add(new User("Kate", 5));
+        users.add(new User("Sam", 5));
+        users.add(new User("Tim", 25));
+
+        User oldestUser = getOldestUser(users);
+        System.out.println(oldestUser);
+    }
+
+
+    public static User getOldestUser(Collection<User> users) {
+        Set<User> uniqueUsers = new HashSet<>(users);
+        List<User> sortedUsers = new ArrayList<>(uniqueUsers);
+        sortedUsers.sort(Comparator.comparing(User::getAge).reversed().thenComparing(User::getName));
+        return sortedUsers.get(0);
     }
 }
